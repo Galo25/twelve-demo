ccd696234b882a24feac150a4fe3fd5e4e738a60
Gal-twelve <gal@twelve-cap.com>
2022-06-10 09:54:33 +0300

    remove plaid

--- manage.py
+++ manage.py
@@ -2,7 +2,6 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-from plaid import Client
 
 def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'financeapp.settings')
