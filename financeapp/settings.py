13df5884ff18cf923b01f767f5e23cbd687fa190
Gal-twelve <gal@twelve-cap.com>
2022-06-10 09:59:49 +0300

    allowed HOSTS

--- financeapp/settings.py
+++ financeapp/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'skljdh$#!^$#GG%$#fsdfsd'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['twelve-demo.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
